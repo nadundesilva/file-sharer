d006f58150e28fb4cf0b4060f06e1e14576ea8fb
nadundesilva <nadunrds@gmail.com>
2017-10-08 19:57:08 +0530

    Add Cross-Origin Resource Sharing support for the api

---
+++ node-server/src/main/java/org/microfuse/file/sharer/node/server/filter/CORSFilter.java
@@ -0,0 +1,40 @@
+package org.microfuse.file.sharer.node.server.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Cross-Origin Resource Sharing Filter.
+ * This filter adds the necessary headers for Cross-Origin Resource Sharing requests.
+ */
+public class CORSFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(CORSFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Do nothing
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        logger.debug("Adding Access Control Response Headers");
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        // Do nothing
+    }
+}
--- node-server/src/main/java/org/microfuse/file/sharer/node/server/utils/Constants.java
+++ node-server/src/main/java/org/microfuse/file/sharer/node/server/utils/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public static final int WEB_APP_PORT = 7000;
     public static final String WEB_APP_DIRECTORY = "app";
     public static final String WEB_APP_API_URL = "/api";
+    public static final String WEB_APP_WEB_SOCKET_URL = "/api/web-socket";
 
     private Constants() {   // Preventing from being initiated
     }
